557fbbfb8b6548f3fee0d880db78e2079c65486b
Isabel <isabel.stewart@nesta.org.uk>
2023-06-13 13:21:42 +0100

    updating with some more refactoring

--- ahl_targets/analysis/product_category_stats.py
+++ ahl_targets/analysis/product_category_stats.py
@@ -18,22 +18,43 @@ store_coding = get_data.store_itemisation_coding()
 store_lines = get_data.store_itemisation_lines()
 
 
-def spend_share_subset(prod_purch_df: pd.DataFrame, category: str, threshold):
-    """Subset purchase/product info by categories with a greater
-    than threshold % spend share.
+def spend_share_subset(
+    prod_purch_df: pd.DataFrame,
+    category: str,
+    threshold: float,
+) -> pd.DataFrame:
     """
+    Subset purchase/product info by categories with a greater than threshold % spend share.
+
+    Args:
+        prod_purch_df (pd.DataFrame): DataFrame with purchase/product information.
+        category (str): Name of the column containing the categories.
+        threshold (float): Minimum threshold for the spend share.
+
+    Returns:
+        pd.DataFrame: Subset of prod_purch_df containing only the categories with
+                      a spend share greater than threshold.
+    """
+    # Calculate the spend share for each category
     spend_share = (
         (prod_purch_df["Gross Up Weight"] * prod_purch_df["Spend"])
         .groupby(prod_purch_df[category])
         .sum()
         / (prod_purch_df["Gross Up Weight"] * prod_purch_df["Spend"]).sum()
     ).reset_index(name="spend_share")
+
+    # Get a list of the categories with spend share greater than the threshold
     spend_list = list(spend_share[spend_share.spend_share > threshold][category])
 
+    # Return a subset of the original DataFrame containing only the selected categories
     return prod_purch_df[prod_purch_df[category].isin(spend_list)].copy()
 
 
-def product_category_report(cat_data: pd.DataFrame, file_name: str, category: str):
+def product_category_report(
+    cat_data: pd.DataFrame,
+    file_name: str,
+    category: str,
+) -> pd.DataFrame:
     """Creates table of metrics based on products per category group and saves as csv file.
 
     Args:
@@ -103,25 +124,45 @@ def product_category_report(cat_data: pd.DataFrame, file_name: str, category: st
 if __name__ == "__main__":
     # Create dataset with complete purchase level info
     prod_purch_df = model_data.purchase_complete(
-        prod_table, pur_rec_vol, gravity, nut_recs, fvn, store_coding, store_lines
+        prod_table,
+        pur_rec_vol,
+        gravity,
+        nut_recs,
+        fvn,
+        store_coding,
+        store_lines,
     )
     # Subset dataset based on stores + manufacturers with >1.5% spend share
-    store_sub_prods = stores.store_subset(prod_purch_df)
+    store_sub_prods = stores.store_subset(
+        prod_purch_df,
+    )
     manuf_sub_prods = spend_share_subset(prod_purch_df, "manufacturer", 0.005)
     manuf_store_sub_prods = prod_purch_df.pipe(
-        spend_share_subset, category="manufacturer", threshold=0.005
-    ).pipe(stores.store_subset)
+        spend_share_subset,
+        category="manufacturer",
+        threshold=0.005,
+    ).pipe(
+        stores.store_subset,
+    )
 
     logging.info("Saving files")
     # store subset products - store
     store_sub_df = product_category_report(
-        store_sub_prods, "store_subset_store_info", "store_cat"
+        store_sub_prods,
+        "store_subset_store_info",
+        "store_cat",
     )
     # store subset products - manufacturer
     store_sub_manuf_df = product_category_report(
-        manuf_store_sub_prods, "store_subset_manuf_info", "manufacturer"
+        manuf_store_sub_prods,
+        "store_subset_manuf_info",
+        "manufacturer",
     )
     # store
-    store_df = product_category_report(prod_purch_df, "store_info", "store_cat")
+    store_df = product_category_report(
+        prod_purch_df,
+        "store_info",
+        "store_cat",
+    )
     # manufacturer
     manuf_df = product_category_report(manuf_sub_prods, "manuf_info", "manufacturer")
