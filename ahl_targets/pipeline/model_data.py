5606c5a7be83bd412e3ba3177608686291c9f9c2
ElenaMariani <elena.mariani@nesta.org.uk>
2023-10-17 16:04:50 +0100

    update pipeline with revised NPM

--- ahl_targets/pipeline/model_data.py
+++ ahl_targets/pipeline/model_data.py
@@ -1,28 +1,21 @@
-from ahl_targets.pipeline import hfss
+from ahl_targets.getters import get_data
 from ahl_targets.pipeline import stores_transformation as stores
 from ahl_targets.pipeline import product_transformation as product
+from nesta_ds_utils.loading_saving.S3 import upload_obj
 import logging
+from ahl_targets import BUCKET_NAME
 
 
-def make_data(
-    prod_table, pur_rec_vol, gravity, nut_recs, fvn, store_coding, store_lines, npm
-):
-    """Merge datasets and restrict to selected retailers
+if __name__ == "__main__":
+    store_coding = get_data.store_itemisation_coding()
+    store_lines = get_data.store_itemisation_lines()
+    pur_rec_vol = get_data.purchase_records_volume()
+    prod_table = get_data.product_metadata()
+    npm = get_data.get_npm()
 
-    Args:
-        prod_table (pd.DataFrame): product metafata
-        pur_rec_vol (pd.DataFrame): purchase record
-        gravity (pd.DataFrame): gravity info for liquids
-        nut_recs (pd.DataFrame): nutritional record
-        fvn (pd.DataFrame): fruit veg and nuts content
-        store_coding (pd.DataFrame): store codes
-        store_lines (pd.DataFrame): store taxonomy
+    logging.info("This script takes about 20 minutes to run")
 
-    Returns:
-        pd.DataFrame: merged data
-    """
-    logging.info("This function takes about 10 minutes to run")
-
+    # define stores to keep
     keep_stores = [
         "Total Tesco",
         "Total Sainsbury's",
@@ -36,162 +29,47 @@ def make_data(
         "Total Iceland",
         "Ocado Internet",
     ]
-
+    # create custom taxonomy
     store_levels = stores.custom_taxonomy(
         stores.taxonomy(
             store_coding,
             store_lines,
         )
     )
-
+    # subset to stores that are part of this analysis
     stores_sub = store_levels[store_levels["store_cat"].isin(keep_stores)]
 
-    logging.info("merge purchase record with product infro")
-    dat1 = hfss.clean_tbl(prod_table, pur_rec_vol)
+    logging.info("merge purchase record with product info")
+    dat1 = pur_rec_vol.merge(
+        prod_table, left_on="Product Code", right_on="product_code"
+    )
 
     logging.info("retain food only")
     dat1 = product.is_food(dat1).query("is_food == 1")
 
     logging.info("filter to kg only")
     # filter to kg only
-    dat2 = hfss.prod_kilos(dat1, nut_recs)
-
-    # generate ED
-    dat2["ed"] = hfss.kcal_per_100g_foods(dat2)
-
-    # remove implausible values
-    prod_kg_nut = dat2[dat2["ed"] < 900].copy()
-
-    # merge with purchase record
-    dat3 = prod_kg_nut.merge(
-        pur_rec_vol[
-            [
-                "PurchaseId",
-                "Period",
-                "Gross Up Weight",
-                "Quantity",
-                "Store Code",
-                "Spend",
-            ]
-        ],
-        on=["PurchaseId", "Period"],
-    )
+    dat2 = dat1.query("reported_volume == 'Kilos'")
 
     logging.info("merge with store data and npm")
 
     # Merge with npm and store names
-    store_data = dat3.merge(
+    store_data = dat2.merge(
         stores_sub, left_on="Store Code", right_on="store_id"
-    ).merge(npm[["PurchaseId", "Period", "npm_score"]], on=["PurchaseId", "Period"])
-
-    return store_data
-
-
-def weighted_kg(store_data):
-    """aggregated df with volume weights
-
-    Args:
-        store_data (pd.DataFrame): merged data
-    """
-    out = (
-        (store_data["volume_up"] * store_data["Gross Up Weight"])
-        .groupby(store_data["product_code"])
-        .sum()
-        .reset_index(name="total_kg")
-    )
-    out["kg_w"] = out["total_kg"] / out["total_kg"].sum()
-
-    return out
-
-
-def weighted_kcal(store_data):
-    """aggregated df with kcal weights
-
-    Args:
-        store_data (pd.DataFrame): merged data
-    """
-    out = (
-        (store_data["Energy KCal"] * store_data["Gross Up Weight"])
-        .groupby(store_data["product_code"])
-        .sum()
-        .reset_index(name="total_kcal")
-    )
-    out["kcal_w"] = out["total_kcal"] / out["total_kcal"].sum()
-
-    return out
-
-
-def weighted_prod(store_data):
-    """aggregated df with product weights
-
-    Args:
-        store_data (pd.DataFrame): merged data
-    """
-    out = (
-        store_data["Gross Up Weight"]
-        .groupby(store_data["product_code"])
-        .sum()
-        .reset_index(name="total_prod")
-    )
-    out["prod_w"] = out["total_prod"] / out["total_prod"].sum()
-
-    return out
-
-
-def unique_ed(store_data):
-    """Unique ED by prodyuct
-
-    Args:
-        store_data (pd.DataFrame): marged data
-
-    Returns:
-        pd.DataFrame: aggregate df by product code with unique ED
-    """
-    return store_data.groupby(["product_code"])["ed"].mean().reset_index(name="ed")
-
-
-def unique_kcal(store_data):
-    """Unique KCAL by prodyuct
-
-    Args:
-        store_data (pd.DataFrame): marged data
-
-    Returns:
-        pd.DataFrame: aggregate df by product code with unique KCAL
-    """
-    return (
-        (store_data["Energy KCal"] / store_data["Quantity"])
-        .groupby(store_data["product_code"])
-        .mean()
-        .reset_index(name="kcal_unit")
+    ).merge(
+        npm[["purchase_id", "period", "npm_score", "kcal_per_100g"]],
+        left_on=["PurchaseId", "Period"],
+        right_on=["purchase_id", "period"],
     )
 
+    store_data.rename(columns={"kcal_per_100g": "ed"}, inplace=True)
 
-def weighted(store_data):
-    """merged aggregated weight data
+    # remove implausible values
+    store_data = store_data[store_data["ed"] < 900].copy()
 
-    Args:
-        store_data (pd.DataFrame): merged data
-
-    Returns:
-        pd.DataFrame: merged aggregated weight data
-    """
-    return (
-        weighted_kg(store_data)
-        .merge(weighted_kcal(store_data), on="product_code")
-        .merge(weighted_prod(store_data), on="product_code")
-        .merge(unique_ed(store_data), on="product_code")
-        .merge(unique_kcal(store_data), on="product_code")
+    upload_obj(
+        store_data,
+        BUCKET_NAME,
+        "in_home/processed/targets/model_data.csv",
+        kwargs_writing={"index": False},
     )
-
-
-def prod_weight_g(w_store_data):
-    """average weight of product in grames
-
-    Args:
-        w_store_data (pd.DataFrame): merged data
-
-    Returns:
-        pd.Series: average weight of product
-    """
-    return w_store_data["total_kg"] / w_store_data["total_prod"] * 1000
