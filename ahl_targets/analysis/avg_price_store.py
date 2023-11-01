45a8e390e0b4b6098f66ecd53fc6dd4afcd0a984
Isabel <isabel.stewart@nesta.org.uk>
2023-11-01 17:48:31 +0000

    updating to include food only and volume weights

--- ahl_targets/analysis/avg_price_store.py
+++ ahl_targets/analysis/avg_price_store.py
@@ -1,35 +1,11 @@
 from ahl_targets.getters import get_data
 from ahl_targets.pipeline import stores_transformation as stores
-from functools import reduce
-import pandas as pd
-from ahl_targets import PROJECT_DIR
 from ahl_targets import BUCKET_NAME
 from nesta_ds_utils.loading_saving.S3 import upload_obj
 
 
 if __name__ == "__main__":
-    prod_table = get_data.product_metadata()
-    pur_rec_vol = get_data.purchase_records_volume()
-    store_coding = get_data.store_itemisation_coding()
-    store_lines = get_data.store_itemisation_lines()
-
-    store_levels = stores.custom_taxonomy(
-        stores.taxonomy(
-            store_coding,
-            store_lines,
-        )
-    )
-
-    # Subset by store
-    store_subset = stores.store_subset(store_levels)
-
-    # Merge with purchase records
-    pur_rec_store = pur_rec_vol.merge(
-        store_subset[["store_id", "store_cat"]],
-        left_on="Store Code",
-        right_on="store_id",
-        how="left",
-    ).dropna(subset=["store_cat"])
+    pur_rec_store = get_data.model_data()
 
     # Calculate the weighted average product per store_cat
     weighted_prods_sold = (
@@ -57,6 +33,20 @@ if __name__ == "__main__":
     )
     weighted_purchases_store.columns = ["store_cat", "Weighted_Avg_Price"]
 
+    # Calculate the weighted average price per store (wieghted by volume)
+    pur_rec_store["volume_weighted"] = (
+        pur_rec_store["volume_up"] * pur_rec_store["Gross Up Weight"]
+    )
+    weighted_purchases_store_volume = (
+        pur_rec_store.groupby("store_cat")
+        .apply(
+            lambda x: (x["Spend"] * x["volume_weighted"]).sum()
+            / x["volume_weighted"].sum()
+        )
+        .reset_index()
+    )
+    weighted_purchases_store_volume.columns = ["store_cat", "Weighted_Avg_Price_Volume"]
+
     # Save to S3
     upload_obj(
         weighted_prods_sold,
@@ -68,5 +58,11 @@ if __name__ == "__main__":
         weighted_purchases_store,
         BUCKET_NAME,
         "in_home/data_outputs/weighted_purchases_store.csv",
+        kwargs_writing={"index": False, "encoding": "utf-8"},
+    )
+    upload_obj(
+        weighted_purchases_store_volume,
+        BUCKET_NAME,
+        "in_home/data_outputs/weighted_purchases_store_volume.csv",
         kwargs_writing={"index": False, "encoding": "utf-8"},
     )
